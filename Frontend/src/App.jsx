430b11505e540aee981c46b85afa5aca2ee5023c
PakshalS <pakshalshah1927@gmail.com>
2024-03-18 20:51:03 +0530

    folder changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import Meeting from './pages/Room/meeting'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
       <Routes>
         <Route path='/' element={<Meeting/>}></Route>
         <Route path='/room/:roomId' element={<Routes/>}></Route>
        </Routes>
-      
   )
 }
 
