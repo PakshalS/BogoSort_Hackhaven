2719af4b9d53b766b65aab51f1a4bb2325a3c25a
PakshalS <pakshalshah1927@gmail.com>
2024-03-19 00:48:15 +0530

    webcam-ml

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import Meeting from './pages/Room/meeting'
-
-
+import Dash from './pages/Home/Dashboard'
+import './webcam';
+import Webcam from './web';
 function App() {
   return (
       <Routes>
         <Route path='/' element={<Meeting/>}></Route>
+        {/* <Route path='/' element={<Webcam></Webcam>}></Route> */}
         <Route path='/room/:roomId' element={<Routes/>}></Route>
        </Routes>
   )
