bc020b2dca927fa183a4f55933461f2971a38c39
Dimsas04 <siddheshshrawne10@gmail.com>
2024-03-19 16:13:55 +0530

    Backend Updates

--- Backend/Reciever.py
+++ Backend/Reciever.py
@@ -17,18 +17,41 @@ mongo = PyMongo(app)
 fs = GridFS(mongo.db)
 db = mongo.db.Login_details
 
-@app.route('/', methods=['POST'])
+
+
+def finder(str):
+    result=db.find({"email":str})
+    document=[doc for doc in result]
+    print(document[1])
+    return document
+
+@app.route('/', methods=['POST','GET'])
 def submit_form():
     data = request.json
     print(data)
     result=data.get('msg')
-    print(result)
-    db.insert_one({
-        "username":result.get('username'),
-        "password":bcrypt.generate_password_hash(result.get('password')).decode('utf-8'),
-        "email":result.get('email')
-    })
-    return jsonify({"message": "Data inserted successfully"}),200
+    #print(result)
+    if result.get('username')!="":
+        db.insert_one({
+            "username":result.get('username'),
+            "password":bcrypt.generate_password_hash(result.get('password')).decode('utf-8'),
+            "email":result.get('email')
+        })
+        db.create_index([("username", 1)], unique=True)
+        return jsonify({"redirect_url": "http://localhost:3000"}), 200
+    else:
+        user=result.get('email')
+        print("YAY")
+        doc=finder(user)
+        if bcrypt.check_password_hash(doc[0].get('password'), result.get('password')):
+            print("CORRECT PASSWORD")
+            return jsonify({"redirect_url": "http://localhost:3000/dashboard"}), 200
+
+        else:
+            print("WRONG PASSWORD")
+            return jsonify({"redirect_url": "http://localhost:3000"}), 400
+
+
 
 
 if(__name__=='__main__'):
