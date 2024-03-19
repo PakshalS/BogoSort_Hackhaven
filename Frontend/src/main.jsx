6ecf219f3a53b2a0f71d8291fbab09af36c4e9f4
Dikshant <dikshantbadawadagi@gmail.com>
2024-03-19 16:57:55 +0530

    update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,8 +5,6 @@ import App from './App.jsx'
 import './index.css'
 import Webcam from './webcam.jsx'
 import {BrowserRouter} from 'react-router-dom'
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
