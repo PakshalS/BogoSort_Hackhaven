5c20225c723496feac8055612bc6678ee518ee6e
Aadi Shetty <106333491+gltichy678@users.noreply.github.com>
2024-03-19 16:32:05 +0530

    Update image moderation API secret and remove unnecessary code.

--- Backend/Detector.py
+++ Backend/Detector.py
@@ -1,26 +1,23 @@
 from flask import *
-from cv2 import imshow, imwrite, VideoCapture, imread
+# from SignUpForm import *
+from cv2 import imshow,imwrite,VideoCapture,imread
 from flask_pymongo import PyMongo
 from gridfs import GridFS
 import cv2
 import urllib.parse
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS,cross_origin
 from flask_bcrypt import Bcrypt
 import requests
 import json
 import uuid
 import base64
-import os
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '87c725f6be51b16e19446e14b59149e7'
+app=Flask(__name__)
+app.config['SECRET_KEY']='87c725f6be51b16e19446e14b59149e7'
 CORS(app)
-bcrypt = Bcrypt(app)
-
-
+bcrypt=Bcrypt(app)
 username = urllib.parse.quote_plus('shettyaadi9')
 password = urllib.parse.quote_plus('aadi@2004')
-
 app.config['MONGO_URI'] = f'mongodb+srv://{username}:{password}@cluster0.v2ff3xl.mongodb.net/Login_details?connectTimeoutMS=30000'
 mongo = PyMongo(app)
 fs = GridFS(mongo.db)
@@ -29,32 +26,32 @@ db = mongo.db.Login_details
 params = {
     'models': 'nudity-2.0,wad,offensive',
     'api_user': '492589490',
-    'api_secret': os.environ.get('IMAGE_MODERATION_API_SECRET')
+    'api_secret': 'amKiry7wLtbAcv8x6hoFPuiURqbKoWP4'
 }
-
-if not params['api_secret']:
-    raise ValueError('IMAGE_MODERATION_API_SECRET environment variable must be set')
 
 @app.route('/', methods=['POST'])
 def submit_form():
     data = request.json
-    result = data.get('msg', '')
+    result = data.get('msg')
+    
+    #decode base64 to image
+    image_data = base64.b64decode(result.split(',')[1])
 
     # Generate a unique filename for the image
     filename = f"{uuid.uuid4()}.png"
 
     # Save the image to GridFS
     with fs.new_file(filename) as f:
-        f.write(base64.b64decode(result.split(',')[1]))
+        f.write(result.get(image_data))
 
     # Insert the username and GridFS file ID into the database
     db.users.insert_one({
-        "username": result.get('username', ''),
+        "username": result.get('username'),
         "image_id": str(f.id)
     })
 
     # Retrieve the image data from GridFS
-    with fs.get(db.users.find_one({"username": result.get('username', '')})["image_id"]) as f:
+    with fs.get(db.users.find_one({"username": result.get('username')})["image_id"]) as f:
         image_data = f.read()
 
     # Send the image data to the API
@@ -62,8 +59,10 @@ def submit_form():
     r = requests.post('https://api.sightengine.com/1.0/check.json', files=files, data=params)
     op = r.json()
 
-    if (op.get("weapon", 0) > 0.5 or op.get("nudity", {}).get("suggestive", 0) > 0.5 or op.get("nudity", {}).get("sexual_activity", 0) > 0.5 or op.get("alcohol", 0) > 0.5 or op.get("offensive", {}).get("middle_finger", 0) > 0.5):
-        return jsonify({"message": "Profanity Detected!!!"}), 
+    if (op.get("weapon") > 0.5 or op.get("nudity").get("suggestive") > 0.5 or op.get("nudity").get("sexual_activity") > 0.5 or op.get("alcohol") > 0.5 or op.get("offensive").get("middle_finger") > 0.5):
+        return jsonify({"message": "Profanity Detected!!!"}), 200
+    else:
+        return jsonify({"message": "No Profanity Detected"}), 200
 
-if(__name__=='__main__'):
-    app.run(debug=True,port=5025)
+if __name__ == '__main__':
+    app.run(debug=True, port=5025) 
