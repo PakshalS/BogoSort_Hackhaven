d0683c2bc13e0e5424fe87d27648c3b2588433f5
Dikshant <dikshantbadawadagi@gmail.com>
2024-03-20 02:57:08 +0530

    base64

--- Frontend/src/pages/Home/profile.jsx
+++ Frontend/src/pages/Home/profile.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 const ImageUploader = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [imagePreview, setImagePreview] = useState(null);
 
  
   const handleFileChange = (event) => {
@@ -18,25 +17,10 @@ const ImageUploader = () => {
     }
   }
 
-  const handleUpload = () => {
-    if (selectedFile) {
-      const formData = new FormData();
-      formData.append('image', selectedFile);
-
-      axios.post('http://localhost:5000/', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      .then((response) => {
-        console.log('Image uploaded successfully:', response.data);
-        // Do something with the response if needed
-      } catch (error) {
-        console.error('Error uploading image:', error);
-        // Handle error
-      });
-    } else {
-      console.warn('No image selected for upload.');
+  const handleSubmit = async () => {
+    if (!selectedFile) {
+      alert('Please select a file');
+      return;
     }
     console.log(imagePreview)
    
