fa2af40781501a0d69d1cdc89d618a25157e2d62
PakshalS <pakshalshah1927@gmail.com>
2024-03-20 14:55:15 +0530

    changes done in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,13 @@ import {Routes, Route} from 'react-router-dom'
 import Meeting from './pages/Room/meeting'
 import Dash from './pages/Home/Dashboard'
 import Signup from './pages/Auth/signup';
-import Canvas from './whiteboard';
 
 export default function App() {
   return (
      <Routes>
         <Route path='/' element={<Signup/>}></Route>
         <Route path='/dashboard' element={<Dash/>}></Route>
-        <Route path='/meeting' element={<>
-          <Meeting/>
-          <Canvas></Canvas>
-        </>}></Route>
+        <Route path='/meeting' element={<Meeting/>}></Route>
        </Routes>
   )
 }
