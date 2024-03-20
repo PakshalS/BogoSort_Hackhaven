ac7039dcddbc086de5247a728d500ef08aa98678
PakshalS <pakshalshah1927@gmail.com>
2024-03-20 13:53:34 +0530

    Message

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ImageUploader from './pages/Home/profile.jsx'
 
 import {BrowserRouter} from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App></App>
+    {/* <App></App> */}
+<ImageUploader></ImageUploader>
     </BrowserRouter> 
   </React.StrictMode>
 )
--- Frontend/src/pages/Room/response.jsx
+++ Frontend/src/pages/Room/response.jsx
@@ -1,3 +1,26 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// const YourComponent = () => {
+//     const [message, setMessage] = useState('');
+
+//     useEffect(() => {
+//         // Make an HTTP GET request to your Flask API endpoint
+//         axios.get('http://localhost:5025/')
+//             .then(response => {
+//                 setMessage(response.data.message);
+//                 // Display the message in an alert
+//                 alert(response.data.message);
+//             })
+//             .catch(error => {
+//                 console.error('Error fetching data:', error);
+//             });
+//     }, []);
+
+//     return null; // Returning null since we're using an alert instead of rendering HTML
+// };
+
+// export default YourComponent;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -5,8 +28,8 @@ const YourComponent = () => {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        // Make an HTTP GET request to your Flask API endpoint
-        axios.get('http://localhost:5025/')
+        // Make an HTTP POST request to your Flask API endpoint
+        axios.post('http://localhost:5025/')
             .then(response => {
                 setMessage(response.data.message);
                 // Display the message in an alert
